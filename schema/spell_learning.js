1bd5ae53ac491bbc52079d7322d6fb94d4e08488
Bropedio <bropedio@gmail.com>
2020-12-14 22:33:43 -0600

    Label spell rates with esper names

--- schema/spell_learning.js
+++ schema/spell_learning.js
@@ -6,7 +6,9 @@ const {
   Struct,
   List,
   Enum,
-  UInt
+  UInt,
+  ParallelList,
+  Literal
 } = require('rom-builder').types;
 
 class SpellRates extends JSONer {
@@ -15,34 +17,38 @@ class SpellRates extends JSONer {
 
     const { data, scheme } = fetch('spells');
     const spells = scheme.type.format(data);
-    const spell_names = spells.reduce((obj, spell, i) => {
-      obj[i] = spell.Name;
-      return obj;
-    }, {});
+    const spell_names = spells.map(spell => spell.Name);
     const spell_enum = new Enum({ 0xFF: 'empty' }, spell_names);
+    const esper_names = spell_names.slice(54, 81);
 
-    this.type = new Reader({
-      offset: 0xD86E00,
-      type: new List({
-        size: 27,
-        type: new Struct([{
-          name: 'Spells',
-          type: new List({
-            size: 5,
-            type: new Struct([{
-              name: 'Rate',
-              type: new UInt(10)
-            }, {
-              name: 'Name',
-              type: spell_enum
-            }])
-          })
-        }, {
-          name: 'Bonus',
-          type: new UInt()
-        }])
+    this.type = new ParallelList([{
+      name: 'Esper',
+      type: new Literal(esper_names)
+    }, {
+      name: 'Data',
+      type: new Reader({
+        offset: 0xD86E00,
+        type: new List({
+          size: 27,
+          type: new Struct([{
+            name: 'Spells',
+            type: new List({
+              size: 5,
+              type: new Struct([{
+                name: 'Rate',
+                type: new UInt(10)
+              }, {
+                name: 'Name',
+                type: spell_enum
+              }])
+            })
+          }, {
+            name: 'Bonus',
+            type: new UInt()
+          }])
+        })
       })
-    });
+    }]);
   }
 }
 
