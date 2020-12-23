e88d9b00f6d09eb806b6e5f71d21446cf5da5470
Bropedio <bropedio@gmail.com>
2020-12-22 20:12:42 -0600

    Expand formations schema

--- schema/formations.js
+++ schema/formations.js
@@ -11,7 +11,10 @@ const {
   UInt,
   List,
   Struct,
+  Bits,
+  Bool,
   Custom,
+  ParallelList,
   JSONer
 } = types;
 
@@ -24,7 +27,100 @@ class Formations extends JSONer {
     const monster_names = get_values(fetch('monsters'));
     const monster_enum = new Enum(monster_names, { 0x1FF: '-' });
 
-    this.type = new Reader({
+    const formation_data = new Reader({
+      offset: 0xCF5900,
+      type: new List({
+        size: 0x240,
+        type: new Struct([{
+          name: 'Flags',
+          type: new Bits([{
+            name: 'Appearance Effects',
+            mask: 0x000F,
+            type: new UInt()
+          }, {
+            name: 'Disable Normal',
+            mask: 0x0010,
+            type: new Bool()
+          }, {
+            name: 'Disable Back',
+            mask: 0x0020,
+            type: new Bool()
+          }, {
+            name: 'Disable Pincer',
+            mask: 0x0040,
+            type: new Bool()
+          }, {
+            name: 'Disable Side',
+            mask: 0x0080,
+            type: new Bool()
+          }, {
+            name: 'UNKNOWN-0x0100',
+            mask: 0x0100,
+            type: new Bool()
+          }, {
+            name: 'Disable Fanfare',
+            mask: 0x0200,
+            type: new Bool()
+          }, {
+            name: 'Allow Joker Doom',
+            mask: 0x0400,
+            type: new Bool()
+          }, {
+            name: 'Can Leap',
+            mask: 0x0800,
+            type: new Bool()
+          }, {
+            name: 'UNKNOWN-0x1000',
+            mask: 0x1000,
+            type: new Bool()
+          }, {
+            name: 'UNKNOWN-0x2000',
+            mask: 0x2000,
+            type: new Bool()
+          }, {
+            name: 'UNKNOWN-0x4000',
+            mask: 0x4000,
+            type: new Bool()
+          }, {
+            name: 'Enable Event Script',
+            mask: 0x8000,
+            type: new Bool()
+          }])
+        }, {
+          name: 'Battle Event Script',
+          type: new UInt()
+        }, {
+          name: 'Flags-2',
+          type: new Bits([{
+            name: 'No Escape',
+            mask: 0x01,
+            type: new Bool()
+          }, {
+            name: 'No Veldt',
+            mask: 0x02,
+            type: new Bool()
+          }, {
+            name: 'Show Attack Type?',
+            mask: 0x04,
+            type: new Bool()
+          }, {
+            name: 'Hide Start Messages',
+            mask: 0x08,
+            type: new Bool()
+          }, {
+            name: 'Battle Music',
+            mask: 0x70,
+            type: new UInt()
+          }, {
+            name: 'Continue Current Music',
+            mask: 0x80,
+            type: new Bool()
+          }])
+        }])
+      })
+    });
+
+    const formation_enemies = new Reader({
       offset: 0xCF6200,
       type: new List({
         size: 0x240,
@@ -110,6 +206,14 @@ class Formations extends JSONer {
         })
       })
     });
+
+    this.type = new ParallelList([{
+      name: 'Enemies',
+      type: formation_enemies
+    }, {
+      name: 'Data',
+      type: formation_data
+    }]);
   }
 }
 
