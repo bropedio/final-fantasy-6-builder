272ddc44be35565a08cbe0fc7f2f402356c8c417
Bropedio <bropedio@gmail.com>
2021-05-19 14:28:34 -0500

    Add schema for fixed width font

---
+++ schema/fixed_width_font.js
@@ -0,0 +1,35 @@
+"use strict";
+
+const {
+  Reader,
+  Tile,
+  List,
+  Closure,
+  JSONer
+} = require('rom-builder').types;
+
+const LZSS = require('./lib/lzss');
+
+class FixedWidthFont extends Closure {
+  constructor (fetch) {
+    super();
+
+    this.type = new Reader({
+      offset: 0xC487C0,
+      type: new List({
+        size: 256,
+        type: new Tile({
+          bpp: 2
+        })
+      })
+    });
+  }
+  parse (string) {
+    return this.type.parse(string.split('\n\n'));
+  }
+  format (data) {
+    return this.type.format(data).join('\n\n');
+  }
+}
+
+module.exports = FixedWidthFont;
